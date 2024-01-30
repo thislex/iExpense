997d8158b5b6f2532a4f6d89f822eb0b24f94430
Lexter Tapawan <lexter.tapawan@gmail.com>
2024-01-30 19:55:36 +0000

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Lexter Tapawan on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
